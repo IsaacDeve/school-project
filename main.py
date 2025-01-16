edbc988873118d9aa5dab668af657625e1056705
Arseniy <156547171+IsaacDeve@users.noreply.github.com>
2025-01-16 14:14:04 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 import random as r
 
-lessons = ["PE", "Computer science", "Math", "Biology", "Media", "Guitar"]
+lessons = ["PE", "Computer science", "Math", "Biology", "Media", "Guitar", "Chemistry", "English"]
 
 current_lessons = []
 banned_indexes = []
 tempnum = 0
-how_many_lessons = 10
+how_many_lessons = 4 #NO MORE THAN 4!!!!
 
 #for l in lessons:
    # print(l)
 
 for l in lessons:
-    if tempnum<=how_many_lessons:
+    if tempnum<=how_many_lessons-1:
         tempnum+=1
         num = r.randint(0, len(lessons)-1)
         current_lessons.append(lessons[num])
